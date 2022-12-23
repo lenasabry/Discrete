5dd786ec2a9aca8ff860e5d5d0a2305e956ad051
lenasabry <121298761+lenasabry@users.noreply.github.com>
2022-12-23 15:41:27 +0200

    Add files via upload

---
+++ prefect number.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//{ 
+  int n1, n2;
+    Console.WriteLine("Enter First Number: ");
+    n1 = int.Parse(Console.ReadLine());
+   
+    Console.WriteLine("Enter Second Number: ");
+    n2 = int.Parse(Console.ReadLine());
+    for (int i = n1; i <= n2; i++)
+    {
+        //int x = 2;
+        if (i % 2 == 2)
+        {
+
+            Console.WriteLine(i + " is a perfect number");
+            Console.WriteLine();
+        }
+
+
+    }
+
+    int n3, n4;
+    Console.WriteLine("Enter First Number: ");
+    n3 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Second Number: ");
+    n4 = int.Parse(Console.ReadLine());
+
+    for (int i = n3; i <= n4; i++)
+    {
+       
+        {
+            Console.WriteLine(i + " is a prefect number");
+            Console.WriteLine();
+        }
+    }
+    Console.ReadKey();
+
+    
---
+++ project prime.cs
@@ -0,0 +1,28 @@
+﻿int num, i, x, y, z;
+Console.Write("pleas enter number1:");
+y = int.Parse(Console.ReadLine())
+
+Console.Write("pleas enter number2:");
+z = int.Parse(Console.ReadLine())
+    Console.Write(The prime numbers between {0} and {1} are:/n",y , z );
+
+    for (num = y ; num <= z; num++) ;
+{
+    x = 0;
+    for (i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            x++;
+            break;
+        }
+    }
+    if (x == 0 && num != 1)
+        Console.Write("{0}", num);
+}
+Console.WriteLine();
+Console.ReadKey();
+}
+{
+}
+
